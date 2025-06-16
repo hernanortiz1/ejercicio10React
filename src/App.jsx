3ee83a9a65b7583812fb1b301dad9cdbc353eecb
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-16 15:20:55 -0300

    agregue contenido a App y baje footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Formulario from "./components/Formulario";
 
 function App() {
- 
-
   return (
     <>
-     
+      <main className="bg-secondary-subtle">
+        <h1 className="my-3 text-center fw-bold">
+          Administrador de Peliculas
+        </h1>
+        <div className="container">
+          <Formulario />
+        </div>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        &copy; Todos los derechos reservados
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
